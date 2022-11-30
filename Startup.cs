b8f82fdc00dee7b2174a353e1dcd724184d1973e
MahmoudMardawi <ma.mardawi@gmail.com>
2022-11-30 11:25:45 +0200

    Register the repo in the startup

--- Startup.cs
+++ Startup.cs
@@ -12,7 +12,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-
+using PlatformService.Data;
 
 namespace PlatformService
 {
@@ -31,6 +31,7 @@ namespace PlatformService
             services.AddDbContext<AppDbContext>(opt => 
             opt.UseInMemoryDatabase("InMem"));
 
+            services.AddScoped<IPlatformRepo, PlatformRepo>();
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
